bee12835536ba9d98e54b5bb4e539e8a3f1bfdcf
marcelordrgs98@gmail.com <marcelordrgs98@gmail.com>
2019-12-17 20:03:09 -0300

    navgation to detail

--- js/list.js
+++ js/list.js
@@ -4,6 +4,13 @@ window.onload = function() {
     //TODO: Ler local storage para popular lista
 }
 
+function listItemClick(event){
+    var option = document.location.href.split("?")[1];
+    var id = event.getAttribute("data-id");
+
+    document.location.href = "detail.html?" + option + "&" + id;
+}
+
 function setTitle(option){
     if(option == 'receber'){
         document.getElementById('titleNavBar').innerText = "A receber";
