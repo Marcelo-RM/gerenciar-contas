1f7a69144ba4338c3f38c4c62f2b60687d01843b
marcelordrgs98@gmail.com <marcelordrgs98@gmail.com>
2019-12-17 20:33:20 -0300

    escopo detail view completo

---
+++ js/detail.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    var option = document.location.href.split("?")[1].split("&")[0];
+    var id = document.location.href.split("&")[1];
+    this.setTitle(option);
+    //TODO: Ler local storage para popular lista
+}
+
+function setTitle(option){
+    if(option == 'receber'){
+        document.getElementById('titleNavBar').innerText = "A receber";
+    } else {
+        document.getElementById('titleNavBar').innerText = "A pagar";
+    }
+}
